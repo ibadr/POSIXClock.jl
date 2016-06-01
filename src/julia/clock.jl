8803909f61383e0917decc205145f315be38347d
Islam <ibadreldin@ufl.edu>
2016-05-31 22:51:34 -0400

    Add nanosleep()

--- src/julia/clock.jl
+++ src/julia/clock.jl
@@ -4,6 +4,11 @@ typealias clockid_t Int32
   CLOCK_MONOTONIC = 1
 )
 
+@enum(TIMER_FLAG,
+  TIMER_RELTIME = 0,
+  TIMER_ABSTIME = 1
+)
+
 type timespec
   sec::Clonglong
   nsec::Clong
@@ -13,5 +18,15 @@ function gettime(clockid::CLOCK_ID)
   res = timespec(0,0)
   s = ccall((:clock_gettime,librt),Int32,(clockid_t,Ref{timespec}),
     clockid,Ref(res))
+  s!=0 && error("Error in gettime()")
   return res
+end
+
+function nanosleep(clockid::CLOCK_ID, t::timespec, flag::TIMER_FLAG = TIMER_ABSTIME)
+  flag==TIMER_RELTIME && error("Relative time nanosleep not supported yet!")
+  rem = timespec(0,0)
+  s = ccall((:clock_nanosleep,librt),Int32,(clockid_t,Int32,Ref{timespec},Ref{timespec}),
+    clockid,flag,Ref(t),Ref(rem))
+  s!=0 && error("Error in nanosleep()")
+  return nothing
 end
